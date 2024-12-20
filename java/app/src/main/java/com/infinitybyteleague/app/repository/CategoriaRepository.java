6f3a771e991614dc3d99e7ef7e373e109012cdcd
Mariana Torres <marith.3289@gmail.com>
2024-12-20 11:58:57 -0600

    fix: :bug: Actualizando carpertas

--- java/app/src/main/java/com/infinitybyteleague/app/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infinitybyteleague.app.repository;
-
-import com.infinitybyteleague.app.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
--- java/app/src/main/java/com/infinitybyteleague/app/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infinitybyteleague.app.repository;
-
-import com.infinitybyteleague.app.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
